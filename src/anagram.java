c2d65565d2a192cd963fc2b4c0579b62846e5f59
Sara-pak <sarabpak@gmail.com>
2022-10-22 22:06:43 -0400

    My daily dose of JAVA.

--- src/anagram.java
+++ src/anagram.java
@@ -1,9 +1,7 @@
 import java.util.Arrays;
 
-public class anagram
-{
-    static void method1(String s1, String s2)
-    {
+public class anagram {
+    static void method1(String s1, String s2) {
         //Removing all white spaces from s1 and s2
         String copyOfs1 = s1.replaceAll("\\s", "");
         String copyOfs2 = s2.replaceAll("\\s", "");
@@ -11,14 +9,9 @@ public class anagram
         //Initially setting status as true
         boolean status = true;
 
-        if(copyOfs1.length() != copyOfs2.length())
-        {
-            //Setting status as false if copyOfs1 and copyOfs2 doesn't have same length
+        if (copyOfs1.length() != copyOfs2.length()) {//Setting status as false if copyOfs1 and copyOfs2 doesn't have same length
             status = false;
-        }
-        else
-        {
-            //Changing the case of characters of both copyOfs1 and copyOfs2 and converting them to char array
+        } else {   //Changing the case of characters of both copyOfs1 and copyOfs2 and converting them to char array
             char[] s1Array = copyOfs1.toLowerCase().toCharArray();
             char[] s2Array = copyOfs2.toLowerCase().toCharArray();
 
@@ -31,18 +24,64 @@ public class anagram
         }
 
         //Output
-        if(status)
-        {
-            System.out.println(s1+" -AND- "+s2+" are Anagrams");
-        }
-        else
-        {
-            System.out.println(s1+" -AND- "+s2+" are -NOT- Anagrams");
+        if (status) {
+            System.out.println(s1 + " -AND- " + s2 + " are Anagrams");
+        } else {
+            System.out.println(s1 + " -AND- " + s2 + " are -NOT- Anagrams");
         }
     }
 
-    public static void main(String[] args)
+    //Check for Anagram using an Iterative method
+    static void method2(String s1m2, String s2m2)
+
+    //Removing white spaces from s1m2 and s2m2 and changing case to lower
+    String copyOfs1m2 = s1m2.replaceAll("\\s", "").toLowerCase();
+    String copyOfs2m2 = s2m2.replaceAll("\\s", "").toLowerCase();
+
+    //Initially setting status as true
+    boolean status = true;
+
+        if(copyOfs1m2.length()!=copyOfs2m2.length())
+
+    {   //Setting status as false if copyOfs1m2 and copyOfs2m2 doesn't have same length
+        status = false;
+    }
+        else
+
+    {  //Converting copyOfs1m2 to char array
+        char[] s1ToArray = copyOfs1m2.toCharArray();
+
+        //Checking whether each character of s1m2ToArray is present in copyOfs2m2
+        for (char c : s1ToArray) {
+            int index = copyOfs2m2.indexOf(c);
+
+            if (index != -1) {
+                //If character is present in copyOfs2m2, removing that char from copyOfs2m2
+                copyOfs2m2 = copyOfs2m2.substring(0, index) + copyOfs2m2.substring(index + 1, copyOfs2m2.length());
+            } else {
+                //If character is not present in copyOfs2m2, setting status as false and breaking the loop
+                status = false;
+                break;
+            }
+        }
+    }
+
+
+    //Output
+
+        if(status)
+
     {
+        System.out.println(s1m2 + " and " + s2m2 + " are anagrams");
+    }
+        else
+
+    {
+        System.out.println(s1m2 + " and " + s2m2 + " are not anagrams");
+    }
+
+}
+    public static void main(String[] args) {
         method1("Mother In Law", "Hitler Woman");
         method1("keEp", "peeK");
         method1("Debit Card", "Bad Credit");
@@ -50,75 +89,7 @@ public class anagram
         method1("DORMITORY", "Dirty Room");
         method1("ASTRONOMERS", "NO MORE STARS");
         method1("Toss", "Shot");
-
-    //Check for Anagram using an Iterative method
-/*
-            {
-                //Removing white spaces from s1 and s2 and changing case to lower
-
-                String copyOfs1 = s1.replaceAll("\\s", "").toLowerCase();
-
-                String copyOfs2 = s2.replaceAll("\\s", "").toLowerCase();
-
-                //Initially setting status as true
-
-                boolean status = true;
-
-                if(copyOfs1.length() != copyOfs2.length())
-                {
-                    //Setting status as false if copyOfs1 and copyOfs2 doesn't have same length
-
-                    status = false;
-                }
-                else
-                {
-                    //Converting copyOfs1 to char array
-
-                    char[] s1ToArray = copyOfs1.toCharArray();
-
-                    //Checking whether each character of s1ToArray is present in copyOfs2
-
-                    for (char c : s1ToArray)
-                    {
-                        int index = copyOfs2.indexOf(c);
-
-                        if(index != -1)
-                        {
-                            //If character is present in copyOfs2, removing that char from copyOfs2
-
-                            copyOfs2 = copyOfs2.substring(0, index)+copyOfs2.substring(index+1, copyOfs2.length());
-                        }
-                        else
-                        {
-                            //If character is not present in copyOfs2, setting status as false and breaking the loop
-
-                            status = false;
-
-                            break;
-                        }
-                    }
-                }
-
-                //Output
-
-                if(status)
-                {
-                    System.out.println(s1+" and "+s2+" are anagrams");
-                }
-                else
-                {
-                    System.out.println(s1+" and "+s2+" are not anagrams");
-                }
-            }
-
-        {
-            method1("Mother In Law", "Hitler Woman");
-            method1("keEp", "peeK");
-            method1("SiLeNt CAT", "LisTen AcT");
-            method1("Debit Card", "Bad Credit");
-            method1("Toss", "Shot");
-            method1("joy", "enjoy");
-        }
-*/
     }
+
+
 }
