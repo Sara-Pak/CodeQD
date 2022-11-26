6238993d069a563ee661ffc83702ebe987619e56
Sara-pak <sarabpak@gmail.com>
2022-11-26 18:10:31 -0500

    finding length of user's string input.

--- src/PYTHON/ButFirstPractice.py
+++ src/PYTHON/ButFirstPractice.py
@@ -1,2 +1,13 @@
-#just print
-print("Hello, Earth")
+#day 1 - python print function
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#find the length of the user's string input
+str = input("What is your name? ")
+print(len(str))
